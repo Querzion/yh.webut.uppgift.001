41ca8a00796117af94148ad958b917a0bb820602
Slisk Lindqvist <slisk.lindqvist@querzion.com>
2024-09-19 08:23:59 +0200

    to tired to continue. Added an accordian and started adding some css. I will probably delete everything and do a fresh start over on the _faq.css file.

--- js/main.js
+++ js/main.js
@@ -1,3 +1,20 @@
+/* https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_accordion */
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
+
 
 /* W3schools and then ChatGPT in order to get it implemented */
 // Dark mode toggle function
