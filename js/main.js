23357be7b749f63089d1f0a20c2b4a4d8cdab7d8
Slisk Lindqvist <slisk.lindqvist@querzion.com>
2024-09-19 19:15:04 +0200

    Lots of comments, and some deletes of commented text, moved commented text in some places, commented full sections and added code to the navbar, it doesn't work as it should, so I'm trying to back-track and fix it in steps.

--- js/main.js
+++ js/main.js
@@ -1,19 +1,3 @@
-/* https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_accordion */
-
-var acc = document.getElementsByClassName("accordion");
-var i;
-
-for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var panel = this.nextElementSibling;
-    if (panel.style.display === "block") {
-      panel.style.display = "none";
-    } else {
-      panel.style.display = "block";
-    }
-  });
-}
 
 
 /* W3schools and then ChatGPT in order to get it implemented */
@@ -50,12 +34,52 @@ function darkMode() {
 // Change the background color of the navigation bar on scroll
 document.addEventListener('scroll', function () {
     const header = document.querySelector('.sticky-header');
-    if (window.scrollY > 50) { // Adjust this value based on when you want the change to happen
+    if (window.scrollY > 50) {
+        console.log('Scrolled more than 50px');
         header.classList.add('scrolled');
     } else {
         header.classList.remove('scrolled');
     }
 });
+
+/* https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_accordion */
+/* var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var panel = this.nextElementSibling;
+        if (panel.style.display === "block") {
+            panel.style.display = "none";
+        } else {
+            panel.style.display = "block";
+    }
+});
+} */
+/* I put this accordion script through ChatGPT, and I hope it was a good change. */
+document.addEventListener("DOMContentLoaded", function() {
+    const acc = document.querySelectorAll(".faq-accordion");
+  
+    acc.forEach(button => {
+      button.addEventListener("click", function() {
+        // Toggle active class on accordion
+        this.classList.toggle("active");
+  
+        // Toggle the panel
+        const panel = this.nextElementSibling;
+        if (panel.style.maxHeight) {
+          panel.style.maxHeight = null;
+        } else {
+          panel.style.maxHeight = panel.scrollHeight + "px";
+        }
+      });
+    });
+  });
+  /* Yup, much smoother! */
+
+
+
 
 
 
